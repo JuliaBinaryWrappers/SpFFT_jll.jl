4350a418640cee4575a106d2b8438f2455be19bc
jlbuild <juliabuildbot@gmail.com>
2026-03-23 14:21:14 +0000

    SpFFT_jll build 1.1.2+0

--- src/SpFFT_jll.jl
+++ src/SpFFT_jll.jl
@@ -8,5 +8,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SpFFT")
-JLLWrappers.@generate_main_file("SpFFT", UUID("8be71ea8-ca6b-526f-84e8-fb2862ef466b"))
+JLLWrappers.@generate_main_file("SpFFT", Base.UUID("8be71ea8-ca6b-526f-84e8-fb2862ef466b"))
 end  # module SpFFT_jll
